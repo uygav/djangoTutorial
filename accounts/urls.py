82b82913c3ffd6e5de0895c25068885974b39d21
Uygar Muntaş <muntasuygar@gmail.com>
2023-09-13 06:48:05 +0300

    urls and views

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # have to access view.py for http def's
+urlpatterns = [
+    path('',views.home ),
+    path('products/',views.products),
+    path('customer/',views.customer)
+    
+]
