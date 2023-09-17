ed74b1343cb5018ef5e3ca6daf40093f88567b09
Uygar Muntaş <muntasuygar@gmail.com>
2023-09-17 23:34:19 +0300

    dynamic url's routing and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views # have to access view.py for http def's
 urlpatterns = [
-    path('',views.home ),
-    path('products/',views.products),
-    path('customer/',views.customer)
+    path('',views.home , name="home"),
+    path('products/',views.products , name='products'),
+    path('customer/<str:pk_test>/',views.customer , name='customer')
 ]
