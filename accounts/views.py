25f111178379364c7188de658641fd94b73f2d81
Uygar Muntaş <muntasuygar@gmail.com>
2023-09-21 21:36:02 +0300

    inline formsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse # for access http
+from django.forms import inlineformset_factory
 from .models import * 
 from .forms import OrderForm
 # Create your views here.
@@ -34,17 +35,20 @@ def customer(request, pk_test):
     context = {'customer':customer ,'orders':orders, 'order_count':order_count } # i can access customer variable in html file
     return render(request , 'accounts/customer.html',context)
 
-def createOrder(request):
-    
-    form = OrderForm()
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    #form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        #form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
 
-    context = {'form':form}
+    context = {'formset':formset}
 
     return render(request,'accounts/order_form.html',context)
 
