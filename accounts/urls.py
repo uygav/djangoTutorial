e3798e9b46b10647616e51ff2bebb0697ef67492
Uygar Muntaş <muntasuygar@gmail.com>
2023-09-25 22:27:24 +0300

    user registration and login authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views # have to access view.py for http def's
 urlpatterns = [
+    path('register/',views.registerPage,name="register"),
+    path('login/',views.loginPage,name="login"),
+    path('logout/',views.logoutUser,name="logout"),
+
     path('',views.home , name="home"),
     path('products/',views.products , name='products'),
     path('customer/<str:pk_test>/',views.customer , name="customer"),
