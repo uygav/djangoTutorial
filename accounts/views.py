3ef69043eed1143f20a9e7151f9db0f93f3d83b2
Uygar Muntaş <muntasuygar@gmail.com>
2023-09-29 23:21:25 +0300

    user profile with one to one relationship

--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,9 @@ def registerPage(request):
                 username = form.cleaned_data.get('username')
                 group = Group.objects.get(name='customer')
                 user.groups.add(group)
+                Customer.objects.create(
+                    user=user
+                )
                 messages.success(request,'account was created for' + username)
                 return redirect('login')
 
@@ -80,8 +83,16 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders':orders ,
+               'total_orders' : total_orders , 'delivered':delivered , 'pending':pending}
     return render(request,'accounts/user.html',context)
 
 @login_required(login_url='login')
